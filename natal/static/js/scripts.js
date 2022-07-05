c573e0f887428f733b2b7acc85a98d52d8657528
Hugo <hugobillemartins@gmail.com>
2022-07-05 16:10:54 -0300

    sla oq eu fiz aqui para ser bem sincero

--- natal/static/js/scripts.js
+++ natal/static/js/scripts.js
@@ -1,13 +1,20 @@
 const slider = tns({
     container: '.my-slider',
     loop: true,
-    items: 3,
+    responsive: {
+      "600": {
+        items: 2
+      },
+    "750":{
+      items: 3
+      }
+    },
     slideBy: 1,
     nav: false,    
     speed: 800,
     autoplayButtonOutput: false,
     mouseDrag: true,
     lazyload: true,
-    controlsContainer: "#customize-controls",
+    controlsContainer: "#customize-controls"
 
   });
